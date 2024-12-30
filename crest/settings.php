73ed4448f877c4dd9029489f26f698e1e06d1387
SOHAM DAS <sohamdas.nest@gmail.com>
2024-12-30 12:51:52 +0530

    added client id and secret

--- crest/settings.php
+++ crest/settings.php
@@ -1,4 +1,5 @@
 <?php
-
+//define('C_REST_CLIENT_ID','local.6715d874586f66.83833902');//Application ID
+//define('C_REST_CLIENT_SECRET','2T9GB0skH0tx6sEJsBixIQFGi0k60ivsHlZX1qVAMcZ0HJCLA2');//Application key
 define('C_REST_WEB_HOOK_URL', 'https://crm.springfieldproperties.ae/rest/8/d79ltxyx6yuvxxye/');
 define('C_REST_BLOCK_LOG', true);
